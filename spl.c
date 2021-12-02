df5d6b17197f5d128d201caca1cb1dcef842ed28
azinum <lukaska61@gmail.com>
2021-12-02 18:55:01 +0100

    Parsing a very trivial and complete grammar

--- spl.c
+++ spl.c
@@ -5,6 +5,11 @@
 #include <stdarg.h>
 #include <assert.h>
 
+typedef enum Error_code {
+  Error = -1,
+  NoError = 0,
+} Error_code;
+
 typedef float f32;
 typedef double f64;
 typedef int32_t i32;
@@ -14,12 +19,19 @@ typedef uint16_t u16;
 typedef int8_t i8;
 typedef uint8_t u8;
 
+#define KB(n) (n * 1024)
+#define MB(n) (KB(n * 1024))
+#define GB(n) (MB(n * 1024))
+
+#define MAX_ERR_SIZE 512
+
 typedef enum Token_type {
+  T_EOF = 0,
+
   T_IDENTIFIER,
   T_NUMBER,
   T_ASSIGN,
-
-  T_EOF,
+  T_SEMICOLON,
 } Token_type;
 
 typedef struct Token {
@@ -44,10 +56,37 @@ typedef enum Ir_code {
   I_ADD,
 } Ir_code;
 
+/* intermidiate representation of the instructions which are to be generated or interpreted */
+typedef struct Ir {
+  Ir_code i;
+} Ir;
+
+/* compile state */
+typedef struct Compile {
+  i32 status;
+} Compile;
+
+#define MAX_INS (1024 * 10)
+
+/*
+  input -> lexer -> parser -> ast? -> intermidiate representation -> interpreter/code generator
+*/
+typedef struct Parser {
+  Lexer l;
+  Ir ins[MAX_INS];
+  u32 ins_count;
+} Parser;
+
+static i32 parser_init(Parser* p, char* source);
+static void parser_error(Parser* p, const char* fmt, ...);
+static i32 parse(Parser* p);
+static i32 parse_statements(Parser* p);
+static i32 parse_statement(Parser* p);
 static void lexer_init(Lexer* l, char* source);
 static void lexer_error(Lexer* l, const char* fmt, ...);
 static void next(Lexer* l);
-inline i32 compare(Token a, const char* b);
+inline i32 compare(Token t, const char* b);
+inline i32 expect(Token t, Token_type type);
 inline Token lexer_read_symbol(Lexer* l);
 inline Token lexer_read_number(Lexer* l);
 inline i32 is_digit(char ch);
@@ -58,25 +97,108 @@ static Token lexer_peek(Lexer* l);
 static void error_printline(FILE* fp, char* source, char* index);
 
 i32 main(i32 argc, char** argv) {
-  (void)lexer_peek; // @Unused
+  const char* source =
+    "hello; 1234;\n"
+    "b;;;;;; 1;\n"
+  ;
+  Parser p;
+  if (parser_init(&p, (char*)source) == NoError) {
+    if (parse(&p) == NoError) {
+      printf("Parsing complete\n");
+    }
+  }
+  return 0;
+}
 
-  Lexer lexer;
-  Lexer* l = &lexer;
-  lexer_init(l, "hello 12\r\nb = 1");
+i32 parser_init(Parser* p, char* source) {
+  lexer_init(&p->l, source);
+  p->ins_count = 0;
+  return NoError;
+}
+
+void parser_error(Parser* p, const char* fmt, ...) {
+  char err_buffer[MAX_ERR_SIZE] = {0};
+  va_list args;
+  va_start(args, fmt);
+  vsnprintf(err_buffer, MAX_ERR_SIZE, fmt, args);
+  va_end(args);
+
+  Lexer* l = &p->l;
+
+  FILE* fp = stderr;
+  fprintf(fp, "[parse-error]: %d:%d: %s", l->token.line, l->token.column, err_buffer);
+  error_printline(fp, l->source, l->index);
+  l->token.type = T_EOF;
+}
+
+i32 parse(Parser* p) {
+  return parse_statements(p);
+}
+
+/*
+ 
+ stmts : stmt
+       | stmts stmt
+       | ';' stmt
+       ;
+*/
+i32 parse_statements(Parser* p) {
+  i32 result = NoError;
   for (;;) {
-    Token t = lexer_next(l);
+    Token t = lexer_next(&p->l);
     switch (t.type) {
       case T_EOF: {
-        goto done;
+        return NoError;
+      }
+      case T_SEMICOLON: {
+        break;
       }
       default: {
-        printf("%d:%d: `%.*s`\n", t.line, t.column, t.length, t.buffer);
+        result = parse_statement(p);
+        if (result != NoError) {
+          goto done;
+        }
         break;
       }
     }
   }
 done:
-  return 0;
+  return result;
+}
+
+/*
+ stmt : identifier ';'
+      | number ';'
+      ;
+*/
+i32 parse_statement(Parser* p) {
+  i32 result = NoError;
+
+  Token t = lexer_peek(&p->l);
+  switch (t.type) {
+    case T_IDENTIFIER: {
+      t = lexer_next(&p->l);
+      if (!expect(t, T_SEMICOLON)) {
+        parser_error(p, "expected `;` semicolon, but got `%.*s`\n", t.length, t.buffer);
+        return Error;
+      }
+      break;
+    }
+    case T_NUMBER: {
+      t = lexer_next(&p->l);
+      if (!expect(t, T_SEMICOLON)) {
+        parser_error(p, "expected `;` semicolon, but got `%.*s`\n", t.length, t.buffer);
+        return Error;
+      }
+      break;
+    }
+    default: {
+      parser_error(p, "expected identifier or number, but got `%.*s`\n", t.length, t.buffer);
+      return Error;
+    }
+  }
+
+  return result;
 }
 
 void lexer_init(Lexer* l, char* source) {
@@ -93,7 +215,6 @@ void lexer_init(Lexer* l, char* source) {
   l->column = 1;
 }
 
-#define MAX_ERR_SIZE 512
 void lexer_error(Lexer* l, const char* fmt, ...) {
   char err_buffer[MAX_ERR_SIZE] = {0};
   va_list args;
@@ -114,14 +235,18 @@ void next(Lexer* l) {
   l->token.column = l->column;
 }
 
-i32 compare(Token a, const char* b) {
+i32 compare(Token t, const char* b) {
   const char* index = b;
-  for (int i = 0; i < a.length; ++i, ++index) {
-    if (a.buffer[i] != *index) {
+  for (int i = 0; i < t.length; ++i, ++index) {
+    if (t.buffer[i] != *index) {
       return 0;
     }
   }
   return (*index == 0);
+}
+
+i32 expect(Token t, Token_type type) {
+  return t.type == type;
 }
 
 Token lexer_read_symbol(Lexer* l) {
@@ -131,7 +256,7 @@ Token lexer_read_symbol(Lexer* l) {
   }
   l->token.length = l->index - l->token.buffer;
   if (compare(l->token, "nop")) {
-
+    assert("token `nop` not implemented" && 0);
   }
   else {
     l->token.type = T_IDENTIFIER;
@@ -182,6 +307,10 @@ Token lexer_next(Lexer* l) {
         l->token.type = T_ASSIGN;
         goto done;
       }
+      case ';': {
+        l->token.type = T_SEMICOLON;
+        goto done;
+      }
       case ' ': case '\t': case '\f': case '\v': {
         break;
       }
@@ -215,6 +344,7 @@ Token lexer_peek(Lexer* l) {
   return l->token;
 }
 
+// FIXME: The arrow points to the end of the offending token, however it should point to the beginning.
 void error_printline(FILE* fp, char* source, char* index) {
   assert(source);
   i32 offset = index - source;
@@ -224,11 +354,11 @@ void error_printline(FILE* fp, char* source, char* index) {
   // Scan to the beginning of the line
   char* at = index;
   while (start_offset < offset) {
+    start_offset++;
     char ch = *(at - start_offset);
     if (ch == '\n' || ch == '\r') {
       break;
     }
-    start_offset++;
   }
   // And then we scan to the end of the line
   do {
