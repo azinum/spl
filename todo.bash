5a63eab0c522fd309fc0441fd582cf0cc2073cce
azinum <lukaska61@gmail.com>
2022-08-29 20:25:04 +0200

    Count number of TODOs found

--- todo.bash
+++ todo.bash
@@ -1,3 +1,8 @@
 #/usr/bin/bash
 
+num_todos=$(grep -sn --color=auto "TODO" src/*.spl spl.spl | wc -l)
+echo "${num_todos} todo(s):"
+cols="$(tput cols)"
+printf "%0.s-" $(seq 1 $cols)
+printf "\n"
 grep -sn --color=auto "TODO" src/*.spl spl.spl
