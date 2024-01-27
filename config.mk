1f89f3fb1473895b29603dd004a6c12631c2b557
azinum <lukaska61@gmail.com>
2024-01-27 16:12:27 +0100

    Do not allocate static memory for local variables

--- config.mk
+++ config.mk
@@ -1,13 +1,8 @@
 # config.mk
 
 PROG=spl
-
 SPL_SRC=${subst .spl,, ${wildcard *.spl}}
-
 SPL_EXAMPLES=${subst .spl,, ${shell find examples/ -type f -name '*.spl'}}
-
 SPL_SRC+=${SPL_EXAMPLES}
-
 INSTALL_DIR=/usr/local/bin
-
 LIB_DIR=/usr/share
